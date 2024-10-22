24985fd1a6f6cf1416743b2c5bdcc6478dc7e98d
rahul <rahul.kumar.149278@gmail.com>
2024-10-22 15:46:57 +0530

    ...

--- eslint.config.js
+++ eslint.config.js
@@ -1,7 +1,13 @@
-import eslint from '@eslint/js';
-import tseslint from 'typescript-eslint';
+import globals from "globals";
+import pluginJs from "@eslint/js";
+import tseslint from "typescript-eslint";
 
-export default tseslint.config(
-  eslint.configs.recommended,
-  ...tseslint.configs.recommended,
-);
+
+export default [
+  {files: ["**/*.{js,mjs,cjs,ts}"]},
+  {languageOptions: { globals: globals.browser }},
+  
+  pluginJs.configs.recommended,
+  ...tseslint.configs.strictTypeChecked,
+  ...tseslint.configs.stylisticTypeChecked,
+];
